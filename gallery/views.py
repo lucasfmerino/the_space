c8504aef887e6f6454d9a0992f6efaae948e0b77
lucasfmerino <lucasfmerino@gmail.com>
2023-09-15 19:26:43 -0300

    image path added

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+# Create your views here.
 
 def index(request):
     return render(request, 'gallery/index.html')
 
-# Create your views here.
+
+def image(request):
+    return render(request, 'gallery/image.html')
