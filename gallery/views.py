0a20f8296d7e9e782cf3a43a6fa29d7829a62bba
lucasfmerino <lucasfmerino@gmail.com>
2023-09-27 17:37:01 -0300

    add dinamic names with dict and loop

--- gallery/views.py
+++ gallery/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+
 def index(request):
-    return render(request, 'gallery/index.html')
+    photo_data = {
+    1: {"name": "Nebulosa de Carina",
+        "legend": "webbtelescope.org / NASA / James Webb"},
+    2: {"name": "Galáxia NGC 1079",
+        "legend": "nasa.org / NASA / Hubble"},
+    }
+    return render(request, 'gallery/index.html', {"index_cards": photo_data})
 
 
 def image(request):
