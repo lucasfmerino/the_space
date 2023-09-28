248ac63317814b2973e54ea11c42e3a215365c89
lucasfmerino <lucasfmerino@gmail.com>
2023-09-27 22:41:50 -0300

    creating data in sqlite with the shell

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Photography(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    legend = models.CharField(max_length=150, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    photo_url = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return f"Photography [name={self.name}]"
