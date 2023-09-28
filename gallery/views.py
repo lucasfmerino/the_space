6a19d6f301c98ffb257ddd04efad759be355bf7e
lucasfmerino <lucasfmerino@gmail.com>
2023-09-27 23:21:06 -0300

    accessing the database

--- gallery/views.py
+++ gallery/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 
+from gallery.models import Photography
+
 # Create your views here.
 
 
 
 def index(request):
-    photo_data = {
-    1: {"name": "Nebulosa de Carina",
-        "legend": "webbtelescope.org / NASA / James Webb"},
-    2: {"name": "Galáxia NGC 1079",
-        "legend": "nasa.org / NASA / Hubble"},
-    }
-    return render(request, 'gallery/index.html', {"index_cards": photo_data})
+
+    # photo_data = {
+    # 1: {"name": "Nebulosa de Carina",
+    #     "legend": "webbtelescope.org / NASA / James Webb"},
+    # 2: {"name": "Galáxia NGC 1079",
+    #     "legend": "nasa.org / NASA / Hubble"},
+    # }
+
+    photographs = Photography.objects.all()
+    return render(request, 'gallery/index.html', {"index_cards": photographs})
 
 
 def image(request):
